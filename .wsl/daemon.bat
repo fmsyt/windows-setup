f729365f2169a1498bde52a9ff188bf37ece6a85
fmsyt <fms20yt@gmail.com>
2022-05-25 16:27:03 +0900

    defined default wsl distribution on startup daemon

--- .wsl/daemon.bat
+++ .wsl/daemon.bat
@@ -1,7 +1,7 @@
 @echo off
 
-wsl -u root -- service cron start
-wsl -u root -- service dbus start
-wsl -u root -- service ssh start
-wsl -u root -- service nginx start
-wsl -u root -- service mysql start
+wsl -d Ubuntu -u root -- service cron start
+wsl -d Ubuntu -u root -- service dbus start
+wsl -d Ubuntu -u root -- service ssh start
+wsl -d Ubuntu -u root -- service nginx start
+wsl -d Ubuntu -u root -- service mysql start
