4f798165342c098c95c3de50c5cc2e8e5c466509
fmsyt <fms20yt@gmail.com>
2023-01-06 13:14:44 +0900

    deleted portforward scripts

--- scripts/wsl/register.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-for /f "tokens=3 delims=\ " %%i in ('whoami /groups^|find "Mandatory"') do set LEVEL=%%i
-if NOT "%LEVEL%"=="High" (
-    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "Start-Process %~f0 -Verb runas"
-    exit
-)
-
-schtasks /create /tn %username%\wsl-startup /tr "%~dp0startup.vbs" /sc onstart /rl highest /delay 0000:05
